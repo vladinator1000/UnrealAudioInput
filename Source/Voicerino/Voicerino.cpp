e7fb73171bb5b51c966ae0060caf725a479a801f
Vlady Veselinov <vladkodmc@gmail.com>
2022-05-15 00:31:27 +0100

    init

---
+++ Source/Voicerino/Voicerino.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Voicerino.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Voicerino, "Voicerino" );
---
+++ Source/Voicerino/VoicerinoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VoicerinoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOICERINO_API AVoicerinoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
